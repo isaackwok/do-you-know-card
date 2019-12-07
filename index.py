111adfc8f7390dbde1fb4553f9699fa2ff7cdb8b
Kwok Chun Ho <kch062522@gmail.com>
2019-12-07 17:16:14 +0800

    enable logging

--- index.py
+++ index.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
 import crawler_dcard as dcard
+import logging
+from flask import Flask, render_template
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
+logging.basicConfig(filename='debug.log', level=logging.DEBUG)
 
 @app.route("/")
 def index():
